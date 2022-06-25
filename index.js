979276c4f4cad2433d05f28e9d70771ee22c2426
Aleksandret977 <107354385+Aleksandret977@users.noreply.github.com>
2022-06-25 20:46:08 +0200

    homework-jsj-day1

---
+++ index.js
@@ -0,0 +1,7 @@
+//Spaghetti alla carbonara recipe
+//ingredient for 4 person
+let spaghetti = 400;
+let guanciale = 250;
+let veryFreshEggYolks = 6;
+let agedGratedPecorinoRomanoCheese = 50;
+let rawBlackPepper = 4;
